2c4e0714ff465e19992becdf480c842872eaf347
Russell H <russelljh723@gmail.com>
2023-07-31 18:10:43 -0700

    adds gitignore and some non-ignored files

---
+++ crypto-chip/src/main/scala/permutation/emit_verilog.scala
@@ -0,0 +1,13 @@
+package permutationEmit
+import chisel3._
+import chisel3.util._
+import permutation._
+import layers._
+
+object permutationMain extends App {
+  println("generating verilog")
+  emitVerilog(new permutation_two_wrapper(), Array("--target-dir", "generated"))
+  emitVerilog(new addition_layer(), Array("--target-dir", "generated"))
+  emitVerilog(new substitution_layer(), Array("--target-dir", "generated"))
+  emitVerilog(new diffusion_layer(), Array("--target-dir", "generated"))
+}
