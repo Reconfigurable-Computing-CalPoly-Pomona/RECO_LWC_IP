4569c46599e4273669eba47dccbfe322e23186f8
Russell H <russelljh723@gmail.com>
2023-09-25 18:38:03 -0700

    removes a repeated emitverlog

--- crypto-chip/src/main/scala/permutation/emit_verilog.scala
+++ crypto-chip/src/main/scala/permutation/emit_verilog.scala
@@ -7,7 +7,6 @@ import ascon._
 
 object permutationMain extends App {
   println("generating verilog")
-  emitVerilog(new permutation_two_wrapper(), Array("--target-dir", "generated"))
   emitVerilog(new addition_layer(), Array("--target-dir", "generated"))
   emitVerilog(new substitution_layer(), Array("--target-dir", "generated"))
   emitVerilog(new diffusion_layer(), Array("--target-dir", "generated"))
@@ -25,7 +24,7 @@ object permutationMain extends App {
   emitVerilog(new double_pipe_diff(), Array("--target-dir", "generated"))
   emitVerilog(new ascon(), Array("--target-dir", "generated"))
   emitVerilog(new diffusion_layer(), Array("--target-dir", "generated")) // Vivado 2022.2: 169 LUT, 0 FF, 5.361ns Total Delay
-  emitVerilog(new substitute_lookup_table(), Array("--target-dir", "generated")) // Vivado 2022.2: 3 LUT, 5.361ns Total Delay
+  emitVerilog(new substitute_lookup_table(), Array("--target-dir", "generated")) // Vivado 2022.2: 3 LUT, 5.361ns Total Delay   Vivado 2021.2 Verilog ROM: 0.5 BRAM, 5.890ns Total Delay
   emitVerilog(new substitution_fifo(), Array("--target-dir", "generated")) // Vivado 2022.2: 48 LUT, 26 FF, 5.828ns Total Delay
   emitVerilog(new substitution_layer(), Array("--target-dir", "generated")) 
   emitVerilog(new substitution_layer_compat(), Array("--target-dir", "generated")) 
