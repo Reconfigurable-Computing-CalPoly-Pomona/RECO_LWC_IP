92d9ed9b2ee1c2b14c75819916da92c2e77121e5
Russell H <russelljh723@gmail.com>
2023-09-27 21:49:20 -0700

    migrates to chisel 5; not sure why things work without additional changes

--- crypto-chip/src/main/scala/permutation/layers.scala
+++ crypto-chip/src/main/scala/permutation/layers.scala
@@ -4,7 +4,6 @@ import chisel3._
 import chisel3.util._
 import scala.math._
 import _root_.permutation.posedge
-import org.scalatest.run
 
 class addition_layer extends Module {
   val io = IO(new Bundle {
