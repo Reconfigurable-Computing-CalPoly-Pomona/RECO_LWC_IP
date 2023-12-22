00dcd6816d2613aed179cc82a5fc6fd2b6df96b5
Russell H <russelljh723@gmail.com>
2023-12-21 17:13:02 -0800

    adds verilator to asyncfifotest

--- crypto-chip/src/test/scala/permutation/asyncFIFOtest.scala
+++ crypto-chip/src/test/scala/permutation/asyncFIFOtest.scala
@@ -5,22 +5,22 @@ import org.scalatest.flatspec.AnyFlatSpec
 import layers._
 import scala.math._
 
-
 class async_test extends AnyFlatSpec with ChiselScalatestTester {
-    "one bit" should "work" in {
-        test(new top()).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
-            
-            dut.io.in.bits.poke(0)
-            dut.io.in.valid.poke(1)
-            dut.io.clockA.step()
-            dut.io.in.valid.poke(0)
-            // dut.io.in.bits.poke(1)
-            dut.io.clockA.step(20)
-            dut.io.clockB.step(10)
+  "one bit" should "work" in {
+    test(new top())
+      .withAnnotations(Seq(WriteVcdAnnotation))
+      .withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
+        dut.io.in.bits.poke(0)
+        dut.io.in.valid.poke(1)
+        dut.io.clockA.step()
+        dut.io.in.valid.poke(0)
+        // dut.io.in.bits.poke(1)
+        dut.io.clockA.step(20)
+        dut.io.clockB.step(10)
 
-            dut.io.out.valid.expect(true)
-            dut.io.out.ready.poke(true)
-            dut.io.out.bits.expect(0)
-        }
-    }
+        dut.io.out.valid.expect(true)
+        dut.io.out.ready.poke(true)
+        dut.io.out.bits.expect(0)
+      }
+  }
 }
